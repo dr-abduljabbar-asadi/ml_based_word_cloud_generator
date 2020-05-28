e8f4a9fcc749ea29a46281538b6c6e763a42446a
Dr. Abduljabbar Asadi <abduljabbar.asadi@gmail.com>
2020-05-28 23:58:20 +0200

    hello

--- word_cloud_generator.py
+++ word_cloud_generator.py
@@ -1,3 +1,7 @@
 hello
 this is test
 why
+
+
+git remote -v origin  https://github.com/dr-abduljabbar-asadi/ml_based_word_cloud_generator.git (fetch)
+origin  https://github.com/dr-abduljabbar-asadi/ml_based_word_cloud_generator.git (push)
