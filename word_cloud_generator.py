3b4de737432dc92bd6457952ebf3f51c9efcca44
Dr. Abduljabbar Asadi <abduljabbar.asadi@gmail.com>
2020-05-29 00:09:59 +0200

    hello new change

--- word_cloud_generator.py
+++ word_cloud_generator.py
@@ -1,7 +1,1 @@
-hello
-this is test
-why
-
-
-git remote -v origin  https://github.com/dr-abduljabbar-asadi/ml_based_word_cloud_generator.git (fetch)
-origin  https://github.com/dr-abduljabbar-asadi/ml_based_word_cloud_generator.git (push)
+Hello here I can start codeing 
